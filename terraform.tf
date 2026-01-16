9f8cd8f21d689318afbe09a1bb43210768c72bb5
servale <38044659+servale@users.noreply.github.com>
2026-01-15 23:12:07 -0500

    restored backend key

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    # key              = "terraform.tfstate"
+    key      = "terraform.tfstate"
     use_oidc = true
     # use_azuread_auth = true
   }
